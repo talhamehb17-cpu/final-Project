1eba466e9272476a072e1062d7ef307c49733067
Talha Mehboob <talhamehb17@gmail.com>
2026-07-16 14:56:36 +0500

    all

--- utils/emailTransporter.js
+++ utils/emailTransporter.js
@@ -1,22 +1,25 @@
 const nodemailer = require('nodemailer');
 
 if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-    console.warn('[Email Transporter] WARNING: EMAIL_USER or EMAIL_PASS is not set in environment variables. OTP emails will fail to send.');
+    console.warn('[Email Transporter] WARNING: EMAIL_USER or EMAIL_PASS is not set');
 }
 
-// Shared nodemailer transporter instance
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    pool: true,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
+    }
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log("[EMAIL VERIFY FAILED]", error);
+    } else {
+        console.log("[EMAIL SERVER READY]");
+    }
 });
 
 module.exports = transporter;
